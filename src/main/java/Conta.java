8c1a84598de7414d614b258fdbf6fb866edb0b04
DaveJosef <josedaaaaavid@gmail.com>
2022-07-08 13:02:15 -0300

    Aplica Refatoração Extract Method em imprimirExtrato

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -42,8 +42,10 @@ public abstract class Conta implements IConta {
         return saldo;
     }
 
-    @Override
-    public String toString() {
-        return String.valueOf(this.numero);
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 }
--- src/main/java/ContaPoupanca.java
+++ src/main/java/ContaPoupanca.java
@@ -7,9 +7,6 @@ public class ContaPoupanca extends Conta {
     @Override
     public void imprimirExtrato() {
         System.out.println("=== Extrato Conta Poupanca ===");
-        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-        System.out.println(String.format("Agencia: %d", this.agencia));
-        System.out.println(String.format("Numero: %d", this.numero));
-        System.out.println(String.format("Saldo: %.2f", this.saldo));
+        super.imprimirInfosComuns();
     }
 }
