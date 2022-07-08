84c50ae8205b3c604f41e42ac98ea63da56d0f3d
DaveJosef <josedaaaaavid@gmail.com>
2022-07-08 12:07:29 -0300

    Adiciona classes ContaPoupanca e ContaCorrente

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -1,11 +1,11 @@
-public class Conta {
+public abstract class Conta {
 
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
 
-    private int numero;
-    private int agencia;
-    private double saldo;
+    protected int numero;
+    protected int agencia;
+    protected double saldo;
 
     public Conta() {
         this.agencia = Conta.AGENCIA_PADRAO;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Conta> contas = new ArrayList<>(){{
-            add(new Conta());
-            add(new Conta());
+            add(new ContaCorrente());
+            add(new ContaPoupanca());
         }};
 
         Banco banco = new Banco();
         banco.setContas(contas);
 
-        System.out.println(banco.getContas());
+        banco.getContas().forEach(System.out::println);
     }
 }
